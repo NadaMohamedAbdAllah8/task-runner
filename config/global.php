c85ecd1784c784cdbd33ea3abf4cfe7dd407f534
nada <nada.mohamed@extech.com>
2022-04-24 20:19:08 +0200

    global constants file

---
+++ config/global.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'paginationRecords' => 3,
+
+    // types of tasks
+    'countWordsId' => 1,
+    'countLinesId' => 2,
+    'countCharactersId' => 3,
+
+    // statuses of tasks
+    'runningStatusId' => 1,
+    'passStatusId' => 2,
+    'failStatusId' => 3,
+
+    // uploaded files path
+    'filesPath' => base_path('storage/app/public/uploadedFiles/'),
+]
+
+?>
